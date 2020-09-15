d9586c124cfef62719a49331b269b8d34dfaf93e
MrTanXin <tanx0512@gmail.com>
2020-09-15 22:22:23 +0800

    add 28

---
+++ 28/28.implement-str-str.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=28 lang=csharp
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        return haystack.IndexOf(needle)
+    }
+}
+// @lc code=end
+
