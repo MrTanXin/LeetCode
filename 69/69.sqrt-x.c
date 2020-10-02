9ea41645a06c98ca2faf803b24c526388db8f95c
MrTanXin <tanx0512@gmail.com>
2020-10-02 09:45:02 +0800

    Modified directory structure

---
+++ 69/69.sqrt-x.c
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=69 lang=c
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+int mySqrt(int x)
+{
+    if (x <= 0)
+        return 0;
+
+    return (int)sqrt(x);
+}
+// @lc code=end
